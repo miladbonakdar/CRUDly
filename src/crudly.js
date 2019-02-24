49ec8b99de586a0f7ec86f3b5b2c407daf93ae05
komeil <komeil.mehranfar@gmail.com>
2019-02-24 15:23:56 +0000

    docs enhanced

--- src/crudly.js
+++ src/crudly.js
@@ -3,15 +3,19 @@
 const gate = require("./gate/gate");
 const defaultActions = require("./defaultActions");
 const actionConfig = require("./gate/actionCreator").createActionConfig;
-//TODO: document needed
+/**
+ * @description crudly lib function that will create your api gate
+ * @param config main config object
+ */
 //copy functions to the crudly object
 let crudly = function(config) {
     return new gate(config);
 };
-//TODO: document needed
+/**
+* @description gate class object that can be used for creatin multiple gates
+* @param config main config object
+*/
 crudly.gate = gate; //gate class that can be use to create new instance for new gate
-//TODO: document needed
 crudly.standardCrudActions = defaultActions; //list of default standard actions you can make with REST
-//TODO: document needed
 crudly.actionConfig = actionConfig; //function to make creating action config easier
 module.exports = crudly;
