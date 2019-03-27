1ff78c188cfecec521f3f27b382e24cbb6a91876
milawd <miladbonak@gmail.com>
2019-03-27 17:33:13 +0430

    utils tests are added

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,92 +1,109 @@
-var { hasOwnProperty } = {},
+const { hasOwnProperty } = {},
     { slice } = [];
+
+const dateDifference = function(dateFrom, dateTo = Date(), type = 's') {
+    type = type.toLowerCase();
+    switch (type) {
+    case 'ms':
+        return dateTo.getTime() - dateFrom.getTime();
+    case 's':
+        return (dateTo.getTime() - dateFrom.getTime()) / 1000;
+    case 'm':
+        return (dateTo.getTime() - dateFrom.getTime()) / 60000;
+    case 'h':
+        return (dateTo.getTime() - dateFrom.getTime()) / 3600000;
+    }
+};
+const error = function(msg) {
+    if (typeof console !== 'undefined') {
+        console.error(msg);
+    }
+};
+const trim = function(str) {
+    return str ? str.replace(/^\s*|\s*$/g, '') : '';
+};
+const trimEnd = function(str, chars) {
+    if (str && chars === undefined) {
+        return str.replace(/\s+$/, '');
+    }
+
+    if (!str || !chars) {
+        return str;
+    }
+
+    return str.replace(new RegExp(`[${chars}]+$`), '');
+};
+const toLower = function(str) {
+    return str ? str.toLowerCase() : '';
+};
+const toUpper = function(str) {
+    return str ? str.toUpperCase() : '';
+};
+const isString = function(val) {
+    return typeof val === 'string';
+};
+const isBoolean = function(val) {
+    return val === true || val === false;
+};
+const isFunction = function(val) {
+    return typeof val === 'function';
+};
+const isObject = function(obj) {
+    return obj !== null && typeof obj === 'object';
+};
+const isPlainObject = function(obj) {
+    return isObject(obj) && Object.getPrototypeOf(obj) == Object.prototype;
+};
+const isBlob = function(obj) {
+    return typeof Blob !== 'undefined' && obj instanceof Blob;
+};
+const isFormData = function(obj) {
+    return typeof FormData !== 'undefined' && obj instanceof FormData;
+};
+const when = function(value, fulfilled, rejected) {
+    var promise = Promise.resolve(value);
+
+    if (arguments.length < 2) {
+        return promise;
+    }
+
+    return promise.then(fulfilled, rejected);
+};
+const each = function(obj, iterator) {
+    var i, key;
+
+    if (Array.isArray(obj)) {
+        for (i = 0; i < obj.length; i++) {
+            iterator.call(obj[i], obj[i], i);
+        }
+    } else if (Object(obj)) {
+        for (key in obj) {
+            if (hasOwnProperty.call(obj, key)) {
+                iterator.call(obj[key], obj[key], key);
+            }
+        }
+    }
+
+    return obj;
+};
+
 module.exports = {
-    dateDifference(dateFrom, dateTo = Date(), type = 's') {
-        type = type.toLowerCase();
-        switch (type) {
-        case 'ms':
-            return dateTo.getTime() - dateFrom.getTime();
-        case 's':
-            return (dateTo.getTime() - dateFrom.getTime()) / 1000;
-        case 'm':
-            return (dateTo.getTime() - dateFrom.getTime()) / 60000;
-        case 'h':
-            return (dateTo.getTime() - dateFrom.getTime()) / 3600000;
-        }
-    },
+    dateDifference,
     inBrowser: typeof window !== 'undefined',
-    error(msg) {
-        if (typeof console !== 'undefined') {
-            console.error(msg);
-        }
-    },
-    trim(str) {
-        return str ? str.replace(/^\s*|\s*$/g, '') : '';
-    },
-    trimEnd(str, chars) {
-        if (str && chars === undefined) {
-            return str.replace(/\s+$/, '');
-        }
-
-        if (!str || !chars) {
-            return str;
-        }
-
-        return str.replace(new RegExp(`[${chars}]+$`), '');
-    },
-    toLower(str) {
-        return str ? str.toLowerCase() : '';
-    },
-    toUpper(str) {
-        return str ? str.toUpperCase() : '';
-    },
-    isString(val) {
-        return typeof val === 'string';
-    },
+    error,
+    trim,
+    trimEnd,
+    toLower,
+    toUpper,
+    isString,
     hasOwnProperty,
     slice,
-    isBoolean(val) {
-        return val === true || val === false;
-    },
-    isFunction(val) {
-        return typeof val === 'function';
-    },
-    isObject(obj) {
-        return obj !== null && typeof obj === 'object';
-    },
-    isPlainObject(obj) {
-        return isObject(obj) && Object.getPrototypeOf(obj) == Object.prototype;
-    },
-    isBlob(obj) {
-        return typeof Blob !== 'undefined' && obj instanceof Blob;
-    },
-    isFormData(obj) {
-        return typeof FormData !== 'undefined' && obj instanceof FormData;
-    },
-    when(value, fulfilled, rejected) {
-        var promise = Promise.resolve(value);
-
-        if (arguments.length < 2) {
-            return promise;
-        }
-
-        return promise.then(fulfilled, rejected);
-    },
-    each(obj, iterator) {
-        var i, key;
-
-        if (Array.isArray(obj)) {
-            for (i = 0; i < obj.length; i++) {
-                iterator.call(obj[i], obj[i], i);
-            }
-        } else if (Object(obj)) {
-            for (key in obj) {
-                if (hasOwnProperty.call(obj, key)) {
-                    iterator.call(obj[key], obj[key], key);
-                }
-            }
-        }
-
-        return obj;
-    }
+    isBoolean,
+    isFunction,
+    isObject,
+    isPlainObject,
+    isBlob,
+    isFormData,
+    when,
+    each
 };
--- test/unit/utils.test.js
+++ test/unit/utils.test.js
@@ -1,6 +1,161 @@
 const utils = require('../../src/utils/utils');
 const { check, checkForException } = require('../checkFunctions');
 
-test('dastan', () => {
-    
-});
+test(
+    'dateDifference test',
+    check(() => {
+        expect(utils.dateDifference).toBeDefined();
+        expect(typeof utils.dateDifference).toBe('function');
+        expect(utils.dateDifference(new Date('2018 3 3'), new Date('2018 3 4'))).toBe(1440 * 60);
+    })
+);
+
+test(
+    'inBrowser test',
+    check(() => {
+        expect(utils.inBrowser).toBeDefined();
+        expect(typeof utils.inBrowser).toBe('boolean');
+    })
+);
+
+test(
+    'error test',
+    check(() => {
+        expect(utils.error).toBeDefined();
+        expect(typeof utils.error).toBe('function');
+    })
+);
+
+test(
+    'trim test',
+    check(() => {
+        expect(utils.trim).toBeDefined();
+        expect(typeof utils.trim).toBe('function');
+        expect(utils.trim('  milad')).toBe('milad');
+    })
+);
+
+test(
+    'trimEnd test',
+    check(() => {
+        expect(utils.trimEnd).toBeDefined();
+        expect(typeof utils.trimEnd).toBe('function');
+        expect(utils.trimEnd('dastan ')).toBe('dastan');
+    })
+);
+
+test(
+    'toLower test',
+    check(() => {
+        expect(utils.toLower).toBeDefined();
+        expect(typeof utils.toLower).toBe('function');
+        expect(utils.toLower('LOW')).toBe('low');
+    })
+);
+
+test(///////
+    'toUpper test',
+    check(() => {
+        expect(utils.toUpper).toBeDefined();
+        expect(typeof utils.toUpper).toBe('function');
+        expect(utils.toUpper('upper')).toBe('UPPER');
+    })
+);
+
+test(
+    'isString test',
+    check(() => {
+        expect(utils.isString).toBeDefined();
+        expect(typeof utils.isString).toBe('function');
+        expect(utils.isString('dfgh')).toBe(true);
+        expect(utils.isString({})).toBe(false);
+    })
+);
+
+test(
+    'hasOwnProperty test',
+    check(() => {
+        expect(utils.hasOwnProperty).toBeDefined();
+        expect(typeof utils.hasOwnProperty).toBe('function');
+    })
+);
+
+test(
+    'slice test',
+    check(() => {
+        expect(utils.slice).toBeDefined();
+        expect(typeof utils.slice).toBe('function');
+    })
+);
+
+test(
+    'isBoolean test',
+    check(() => {
+        expect(utils.isBoolean).toBeDefined();
+        expect(typeof utils.isBoolean).toBe('function');
+        expect(utils.isBoolean(true)).toBe(true);
+        expect(utils.isBoolean('true')).toBe(false);
+    })
+);
+
+test(
+    'isFunction test',
+    check(() => {
+        expect(utils.isFunction).toBeDefined();
+        expect(typeof utils.isFunction).toBe('function');
+        expect(utils.isFunction(()=>{})).toBe(true);
+        expect(utils.isFunction({})).toBe(false);
+    })
+);
+
+test(
+    'isObject test',
+    check(() => {
+        expect(utils.isObject).toBeDefined();
+        expect(typeof utils.isObject).toBe('function');
+        expect(utils.isObject({})).toBe(true);
+        expect(utils.isObject('')).toBe(false);
+    })
+);
+
+test(
+    'isPlainObject test',
+    check(() => {
+        expect(utils.isPlainObject).toBeDefined();
+        expect(typeof utils.isPlainObject).toBe('function');
+        expect(utils.isPlainObject({})).toBe(true);
+        expect(utils.isPlainObject(new Date())).toBe(false);
+    })
+);
+
+test(
+    'isBlob test',
+    check(() => {
+        expect(utils.isBlob).toBeDefined();
+        expect(typeof utils.isBlob).toBe('function');
+    })
+);
+
+test(
+    'isFormData test',
+    check(() => {
+        expect(utils.isFormData).toBeDefined();
+        expect(typeof utils.isFormData).toBe('function');
+    })
+);
+
+test(
+    'when test',
+    check(() => {
+        expect(utils.when).toBeDefined();
+        expect(typeof utils.when).toBe('function');
+    })
+);
+
+test(
+    'each test',
+    check(() => {
+        expect(utils.each).toBeDefined();
+        expect(typeof utils.each).toBe('function');
+    })
+);
