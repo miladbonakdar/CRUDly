74eb90751b3710456a9eb9db53e3803170cc841a
milawd <miladbonak@gmail.com>
2019-04-01 00:24:49 +0430

    crudly for browser added

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: path.resolve(__dirname, 'index.js'),
+    entry: ['@babel/polyfill', path.resolve(__dirname, 'index.js')],
     watch: true,
     module: {
         rules: [
